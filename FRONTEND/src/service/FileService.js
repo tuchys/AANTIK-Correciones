039d81603c568522244fe99b7159a31bb95f19d7
Gabriel <cuervo.gabriel@hotmail.com>
2022-11-27 19:29:22 -0500

    Carga masiva
    Se actualizo el front en l prte de carga masiva, se necesita los metodos para llamarlos pero en si ya logra enviar al back el file seleccionado, cambio en diseno

--- FRONTEND/src/service/FileService.js
+++ FRONTEND/src/service/FileService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 export default class FileService {
 
-    url = "http://localhost:8080/";
+    url = "http://localhost:8080/api/benchmarking/upload";
 
     getAll() {
         return axios.get(this.url + "all");
